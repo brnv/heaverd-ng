aaa745ce3e461ddbad1061f71cf86c846230c456
Alexey Baranov <a.baranov@office.ngs.ru>
2014-07-04 13:36:03 +0700

    HEAVER-45: Реализовать часть REST API на go
    Рефакторинг

--- main.go
+++ main.go
@@ -9,21 +9,14 @@ import (
 	"regexp"
 )
 
-const (
-	HOST_RE      = `^/h/([A-Za-z0-9_-]*)[/]?([A-Za-z0-9_-]*)$`
-	CONTAINER_RE = `^/c/([A-Za-z0-9_-]*)$`
-)
-
 var (
-	hosts = make(map[string]*Statistics)
+	hosts        = make(map[string]*Statistics)
+	HOST_RE      = regexp.MustCompile(`^/h/([A-Za-z0-9_-]*)[/]?([A-Za-z0-9_-]*)$`)
+	CONTAINER_RE = regexp.MustCompile(`^/c/([A-Za-z0-9_-]*)$`)
 )
 
 type Statistics struct {
-	Alive bool `json:"alive"`
-	Boxes map[string]struct {
-		Active bool                `json:"active"`
-		Ips    map[string][]string `json:"ips"`
-	} `json:"boxes"`
+	Alive    bool               `json:"alive"`
 	Fs       int                `json:"fs"`
 	IpsFree  int                `json:"ips_free"`
 	La       map[string]float32 `json:"la"`
@@ -33,6 +26,10 @@ type Statistics struct {
 	Ram      map[string]int     `json:"ram"`
 	Score    float32            `json:"score"`
 	Stale    bool               `json:"stale"`
+	Boxes    map[string]struct {
+		Active bool                `json:"active"`
+		Ips    map[string][]string `json:"ips"`
+	} `json:"boxes"`
 }
 
 type Context struct{}
@@ -42,14 +39,28 @@ func handleHelpRequest(w web.ResponseWriter, r *web.Request) {
 }
 
 func handleStatisticsRequest(w web.ResponseWriter, r *web.Request) {
-	stats, _ := json.Marshal(hosts)
+	stats, err := json.Marshal(hosts)
+
+	if err != nil {
+		http.Error(w, "", 500)
+		fmt.Fprintf(w, "%s", err)
+		return
+	}
+
 	fmt.Fprintf(w, "%s", stats)
 }
 
 func handleHostListRequest(w web.ResponseWriter, r *web.Request) {
-	http.Error(w, "", 300)
-	stats, _ := json.Marshal(hosts)
+	stats, err := json.Marshal(hosts)
+
+	if err != nil {
+		http.Error(w, "", 500)
+		fmt.Fprintf(w, "%s", err)
+		return
+	}
+
 	fmt.Fprintf(w, "%s", stats)
+	http.Error(w, "", 300)
 }
 
 func handleHostPing(w web.ResponseWriter, r *web.Request) {
@@ -57,16 +68,18 @@ func handleHostPing(w web.ResponseWriter, r *web.Request) {
 }
 
 func handleHostCreateRequest(w web.ResponseWriter, r *web.Request) {
-	var restQuery = regexp.MustCompile(HOST_RE).FindStringSubmatch(r.URL.Path)
-	hostname := restQuery[1]
+	hostname := HOST_RE.FindStringSubmatch(r.URL.Path)[1]
 
 	rawdata, err := ioutil.ReadAll(r.Body)
-
-	//TODO if rawdata is empty, send 400 code
 
 	if err != nil {
 		http.Error(w, "Error while reading request body", 500)
 		fmt.Fprintf(w, "%s", err)
+		return
+	}
+
+	if string(rawdata) == "" {
+		http.Error(w, "", 400)
 		return
 	}
 
@@ -79,14 +92,29 @@ func handleHostCreateRequest(w web.ResponseWriter, r *web.Request) {
 }
 
 func handleHostInformationRequest(w web.ResponseWriter, r *web.Request) {
-	http.Error(w, "", 501)
+	hostname := HOST_RE.FindStringSubmatch(r.URL.Path)[1]
+
+	hostinfo, err := json.Marshal(hosts[hostname])
+
+	if err != nil {
+		http.Error(w, "", 500)
+		fmt.Fprintf(w, "%s", err)
+		return
+	}
+
+	if string(hostinfo) == "null" {
+		http.Error(w, "", 404)
+		return
+	}
+
+	fmt.Fprintf(w, "%s", hostinfo)
 }
 
 func handleHostOperationRequest(w web.ResponseWriter, r *web.Request) {
 	http.Error(w, "", 501)
 }
 
-func handleFindContainerHostRequest(w web.ResponseWriter, r *web.Request) {
+func handleFindHostByContainerRequest(w web.ResponseWriter, r *web.Request) {
 	http.Error(w, "", 501)
 }
 
@@ -105,7 +133,7 @@ func main() {
 		Put("/h/:hid", handleHostCreateRequest).
 		Get("/h/:hid", handleHostInformationRequest).
 		Post("/h/:hid", handleHostOperationRequest).
-		Get("/c/:cid", handleFindContainerHostRequest).
+		Get("/c/:cid", handleFindHostByContainerRequest).
 		Post("/c/:cid", handleContainerCreateRequest)
 
 	http.ListenAndServe("192.168.4.84:8081", router)
