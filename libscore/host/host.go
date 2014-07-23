941c5361792fa0075452ddc0aad6e122c8fabb77
Alexey Baranov <a.baranov@office.ngs.ru>
2014-07-23 12:55:32 +0700

    HEAVER-46: Рефакторинг кода из ветки performance
    Разделил логику хоста и модуля score

---
+++ libscore/host/host.go
@@ -0,0 +1,52 @@
+package host
+
+import (
+	"heaverd-ng/libstats/linux"
+	"heaverd-ng/libstats/zfs"
+)
+
+type Host struct {
+	Hostname      string
+	CpuUsage      int
+	CpuCapacity   int
+	DiskUsage     int
+	DiskCapacity  int
+	RamUsage      int
+	RamCapacity   int
+	ZfsArcMax     int
+	ControlOpTime int
+	Uptime        int
+}
+
+// refresh method takes 1sec to complete operation, for determining current cpu usage
+func (host *Host) Refresh() (err error) {
+	host.Hostname, err = linux.HostName()
+	if err != nil {
+		return err
+	}
+	host.ZfsArcMax, err = zfs.ArcMax()
+	if err != nil {
+		return err
+	}
+	CpuCapacity, CpuUsage, err := linux.Cpu()
+	if err != nil {
+		return err
+	}
+	host.CpuUsage = (CpuUsage + host.CpuUsage) / 2
+	host.CpuCapacity = CpuCapacity
+	host.DiskCapacity, host.DiskUsage, err = linux.Disk()
+	if err != nil {
+		return err
+	}
+	host.RamCapacity, host.RamUsage, err = linux.Memory()
+	if err != nil {
+		return err
+	}
+	host.Uptime, err = linux.Uptime()
+	if err != nil {
+		return err
+	}
+	// TODO: determine control operation time
+	host.ControlOpTime = 2
+	return err
+}
