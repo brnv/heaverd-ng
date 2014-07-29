cce7c39d404855259866720ef826bae67f51de5e
Alexey Baranov <a.baranov@office.ngs.ru>
2014-07-29 16:51:21 +0700

    HEAVER-46: Рефакторинг кода из ветки performance

--- libscore/score.go
+++ libscore/score.go
@@ -20,77 +20,66 @@ type Profile struct {
 var defaultProfile = Profile{
 	ReservedCPU:          100,
 	ReservedDiskCapacity: 0.1,
-	ReservedRAM:          0, //1048576,
+	ReservedRAM:          1048576,
 	SlowOpThreshold:      300,
 	LagReactionSpeed:     120,
 	UptimeFactor:         130,
 }
 
 type Segment struct {
-	X, Y float64
+	Hostname string
+	Score    float64
+	X, Y     float64
 }
 
-func Hash(input string) float64 {
-	hashsum := sha1.Sum([]byte(input))
-	ret := uint32(hashsum[0]) + uint32(hashsum[1])*(1<<8)
-	ret = ret % 1000
-	return float64(ret) / 1000.0
-}
+type HostsRange []Segment
 
-// calculate host segments
-func CalculateSegments(input map[string]*Host) map[string]*Segment {
-	slice := make([]string, len(input))
-	Segments := make(map[string]*Segment)
-	sum := 0.0
-	shift := 0.0
-	count := 0
-	// get all legnths and summary the segment
-	for name, host := range input {
-		Segments[name] = &Segment{X: 0.0, Y: calculate(host, defaultProfile)}
-		sum += Segments[name].Y
-		slice[count] = name
-		count += 1
+func (s HostsRange) Len() int           { return len(s) }
+func (s HostsRange) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s HostsRange) Less(i, j int) bool { return s[i].Hostname < s[j].Hostname }
+
+func Segments(hosts []Host) []Segment {
+	Segments := []Segment{}
+	scoreSum := 0.0
+	for _, host := range hosts {
+		score := calculate(host, defaultProfile)
+		Segments = append(Segments, Segment{Hostname: host.Hostname, Score: score})
+		scoreSum += score
 	}
-
-	sort.Strings(slice)
-
-	for i := range slice {
-		// let the left point of segment be the right point of previous segment
-		// if it's first segment, shift will be 0
-		Segments[slice[i]].X = shift
-		Segments[slice[i]].Y = Segments[slice[i]].Y/sum + shift
-		shift = Segments[slice[i]].Y
+	sort.Sort(HostsRange(Segments))
+	shift := 0.0
+	for i := range Segments {
+		Segments[i].X = shift
+		Segments[i].Y = Segments[i].Score/scoreSum + shift
+		shift = Segments[i].Y
 	}
 	return Segments
 }
 
-func ChooseHost(container string, fragmentation map[string]*Segment) (host string, err error) {
-	// get float value which belongs to [0;1]
-	cval := Hash(container)
-	for name, segment := range fragmentation {
+func ChooseHost(container string, segments []Segment) (host string, err error) {
+	cval := hash(container)
+	for _, segment := range segments {
 		if cval >= segment.X && cval <= segment.Y {
-			return name, nil
+			return segment.Hostname, nil
 		}
 	}
 	return "", errors.New(
 		fmt.Sprintf("Cannot assign any host to container name %v", container))
-
 }
 
-func calculate(host *Host, profile Profile) float64 {
+func calculate(host Host, profile Profile) float64 {
 	cpuWeight := 1.0 - minNorm(host.CpuUsage, host.CpuCapacity-profile.ReservedCPU)
 	diskWeight := 1.0 - minNorm(int(float32(host.DiskCapacity)*profile.ReservedDiskCapacity), host.DiskUsage)
 	ramWeight := 1 - minNorm(host.RamUsage, host.RamCapacity-host.ZfsArcMax-profile.ReservedRAM)
 	uptimeFactor := 2 * math.Atan(float64(host.Uptime)/float64(profile.UptimeFactor)) / math.Pi
-
-	speedFactor := 1 - 2*math.Atan(math.Max(0, float64(host.ControlOpTime-profile.SlowOpThreshold))/float64(profile.LagReactionSpeed))
+	speedFactor := 1 - 2*math.Atan(math.Max(0, float64(host.ControlOpTime-
+		profile.SlowOpThreshold))/float64(profile.LagReactionSpeed))
 
 	score := cpuWeight * diskWeight * ramWeight * speedFactor * uptimeFactor
 
 	return score
 }
 
-// normalization by minimal argument
 func minNorm(a, b int) float64 {
 	var min int
 	if a < b {
@@ -99,4 +88,11 @@ func minNorm(a, b int) float64 {
 		min = b
 	}
 	return float64(min) / float64(b)
+}
+
+func hash(input string) float64 {
+	hashsum := sha1.Sum([]byte(input))
+	ret := uint32(hashsum[0]) + uint32(hashsum[1])*(1<<8)
+	ret = ret % 1000
+	return float64(ret) / 1000.0
 }
--- perfomancetest.go
+++ perfomancetest.go
@@ -7,66 +7,40 @@ import (
 	"time"
 )
 
-var a, b, c libscore.Host
-
-func LocalRefreshRoutine() {
-	// NOTE: MEM_MIN in kbytes
-	a = libscore.Host{}
-	b = libscore.Host{}
-	c = libscore.Host{}
-
-	// testing
-	for {
-		go func() {
-			err := a.Refresh()
-			if err != nil {
-				fmt.Printf("ERROR OCCURED: %v\n", err)
-				os.Exit(1)
-			}
-		}()
-		go func() {
-			err := b.Refresh()
-			if err != nil {
-				fmt.Printf("ERROR OCCURED: %v\n", err)
-				os.Exit(1)
-			}
-		}()
-		go func() {
-			err := c.Refresh()
-			if err != nil {
-				fmt.Printf("ERROR OCCURED: %v\n", err)
-				os.Exit(1)
-			}
-		}()
-		time.Sleep(time.Second)
-	}
-}
-
 func main() {
-	go LocalRefreshRoutine()
-	name := os.Args[1]
-	Hosts := make(map[string]*libscore.Host)
-	Hosts["a"] = &a
-	Hosts["b"] = &b
-	Hosts["c"] = &c
+	Hosts := make([]libscore.Host, 3)
+	HostA := libscore.Host{}
+	HostB := libscore.Host{}
+	HostC := libscore.Host{}
 
+	go func() {
+		for {
+			HostA.Refresh()
+			HostB.Refresh()
+			HostC.Refresh()
+			Hosts[0] = HostA
+			Hosts[1] = HostB
+			Hosts[2] = HostC
+			time.Sleep(time.Second)
+		}
+	}()
+
+	name := os.Args[1]
 	for {
 		time.Sleep(time.Second)
 		fmt.Printf("============tick===========\n")
-		//for i, host := range Hosts {
-		//	fmt.Printf("%v: Host %v: length: %v\n", i, host.Hostname, host.GetLength())
-		//}
-		Segments := libscore.CalculateSegments(Hosts)
+
+		Segments := libscore.Segments(Hosts)
+
 		for i, seg := range Segments {
 			fmt.Printf("%v: segment: %+v\n", i, seg)
 		}
 		host, err := libscore.ChooseHost(name, Segments)
-		fmt.Printf("Container %v has hash: %v\n", name, libscore.Hash(name))
+
 		if err != nil {
 			fmt.Printf("Error occured: %v\n", err)
 		} else {
 			fmt.Printf("Choosedhost: %v\n", host)
 		}
 	}
-
 }
